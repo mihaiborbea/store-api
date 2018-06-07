e70dea8503887785f93c01073daaf6a780b9882f
Bogdan Anchidin <bogdan.anchidin@anais.digital>
2018-06-08 00:10:31 +0300

    fix file download

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Static folder
-app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/', indexRouter);
